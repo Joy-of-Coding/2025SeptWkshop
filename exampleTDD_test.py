a0f384db504da20b908ec847a828aecb721c2661
Katrina Wright <77311100+KatrinaWright@users.noreply.github.com>
2025-09-27 11:54:25 -0500

    Initialize GREEN-003

---
+++ exampleTDD_test.py
@@ -0,0 +1,10 @@
+from exampleTDD import application
+
+def test_exists():
+    assert application("try this") == 2
+
+def test_empty():
+    assert application() == 0
+
+def test_long():
+    assert application("Mississippi dreams a dream today") == 5
